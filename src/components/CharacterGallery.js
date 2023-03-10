fa9cde548eeb23a70039e87b508a17c8ae121c5c
unknown <amarmalde97@gmail.com>
2023-03-10 18:28:13 +0000

    added in map function

--- src/components/CharacterGallery.js
+++ src/components/CharacterGallery.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import Character from './Character';
-import peopleInfo from "../data/characterData.json"
+import React from "react";
+import Character from "./Character";
 
-function CharacterGallery() {
+import peopleInfo from "../data/characterData.json";
 
-    
+const CharacterGallery = () => {
+    const characterComponentArray = peopleInfo.map(
+        c => <Character {...c} key={c._id} />
+    )
 
-  return (
-    <div data-test='component-char-gallery'>
-        
-    </div>
-  );
+    return (
+        <div data-test="component-char-gallery">
+            {characterComponentArray}
+
+        </div>
+    )
 }
 
 export default CharacterGallery;
